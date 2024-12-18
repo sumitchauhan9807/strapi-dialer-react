c2bee3f7f372ce5c4e150a89437838641b1cbda0
Sumit chauhan <sumitchauhan9807666@gmail.com>
2024-12-18 14:39:11 +0530

    ads

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ function App() {
                     <div className="elementor-widget-container">
                       <style dangerouslySetInnerHTML={{__html: "/*! elementor - v3.18.0 - 08-12-2023 */\n                    .elementor-heading-title{padding:0;margin:0;line-height:1}.elementor-widget-heading .elementor-heading-title[class*=elementor-size-]>a{color:inherit;font-size:inherit;line-height:inherit}.elementor-widget-heading .elementor-heading-title.elementor-size-small{font-size:15px}.elementor-widget-heading .elementor-heading-title.elementor-size-medium{font-size:19px}.elementor-widget-heading .elementor-heading-title.elementor-size-large{font-size:29px}.elementor-widget-heading .elementor-heading-title.elementor-size-xl{font-size:39px}.elementor-widget-heading .elementor-heading-title.elementor-size-xxl{font-size:59px}\n                  " }} />
                       <h1 className="elementor-heading-title elementor-size-default">The All in One<br /><span className="big-text"><b>AI-Powered</b><br /><b>
-                          </b></span>Contact Center Software
+                          </b></span>Global Dialer
                       </h1>
                     </div>
                   </div>
